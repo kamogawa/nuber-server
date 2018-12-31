e7d40e8c4bd806b35c131ea1fd3ea5d7e0d0ae5a
Beumgoo <captain_kbg@naver.com>
2018-12-31 13:31:00 +0900

    Sending Confirmation Email_2

--- src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
+++ src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
@@ -2,6 +2,7 @@ import { Resolvers } from "../../../types/resolvers";
 import { EmailSignUpMutationArgs, EmailSignInResponse } from "../../../types/graph";
 import User from "../../../entities/User";
 import createJWT from "../../../utils/createJWT";
+import Verification from "../../../entities/Verification";
 
 const resolvers: Resolvers = {
     Mutation: {
@@ -17,6 +18,12 @@ const resolvers: Resolvers = {
                     };
                 } else {
                     const newUser = await User.create({...args}).save();
+                    if(newUser.email) {
+                        const emailVerification = await Verification.create({
+                            payload: newUser.email,
+                            target: "EMAIL"
+                        });
+                    }
                     const token = createJWT(newUser.id);
                     return {
                         ok: true,
--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -4,3 +4,19 @@ const mailGunClient = new Mailgun({
     apiKey: process.env.MAILGUN_API_KEY || "",
     domain: "sandboxb9637c1fd697462b83db2263b17daea5.mailgun.org"
 });
+
+const sendEmail = ( subject:string, html: string ) => {
+    const emailData = {
+        from: "captain_kbg@naver.com",
+        to: "captain_kbg@naver.com",
+        subject,
+        html
+    };
+    return mailGunClient.messages().send(emailData);
+};
+
+export const sendVerificationEmail = (fullName:string, key:string) => {
+    const emailSubjecgt = `Hello ${fullName}, please verify your email`;
+    const emailBody = `Verifiy your email by clicking <a href="http://nuber.com/verification/${key}/">here</a>`;
+    return sendEmail(emailSubjecgt, emailBody);
+};
