454a4aa41dcc8678f6c555c96a4ba30467904bde
Beumgoo <captain_kbg@naver.com>
2019-01-15 23:02:53 +0900

    GetNearbyDrivers Resolver 002

--- src/api/User/GetNearbyDrivers/GetNearbyDrivers.resolvers.ts
+++ src/api/User/GetNearbyDrivers/GetNearbyDrivers.resolvers.ts
@@ -1,10 +1,32 @@
 import { Resolvers } from "../../../types/resolvers";
-import privateResolver from "src/utils/privateResolver";
+import privateResolver from "../../../utils/privateResolver";
+import { GetNearbyDriversResponse } from "../../../types/graph";
+import User from "../../../entities/User";
+import { Between, getRepository } from "typeorm";
 
 const resolvers: Resolvers ={ 
     Query: {
-        GetNearByDrivers: privateResolver(async(_, __, {req}) => Promise<Getnear> {
-
+        GetNearByDrivers: privateResolver(async(_, __, {req}): Promise<GetNearbyDriversResponse> =>{
+            const user: User = req.user;
+            const { lastLat, lastLng } = user;
+            try {
+                const drivers: User[] = await getRepository(User).find({
+                    isDriving: true,
+                    lastLat: Between(lastLat-0.05, lastLat +0.05),
+                    lastLng: Between(lastLng-0.05, lastLng +0.05)
+                });
+                return {
+                    ok: true,
+                    error: null,
+                    drivers
+                };
+            } catch (error) {
+                return {
+                    ok: false,
+                    error: error.message,
+                    drivers: null
+                };
+            }
         })
     }
 };
