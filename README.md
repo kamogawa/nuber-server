0672b58e1cd6895c09625796a6bf441db310aa36
Beumgoo <captain_kbg@naver.com>
2019-01-27 22:35:33 +0900

    GetChat Resolver bug fix

--- README.md
+++ README.md
@@ -37,7 +37,7 @@ GraphQL, TypeScript, NodeJS
 - [x] Get Ride
 - [x] Subscribe to Ride Status
 - [x] Create a Chat Room
-- [ ] Get Chat Room Messages
+- [x] Get Chat Room Messages
 - [ ] Subscribe to Chat Room Messages
 - [ ] Send a Chat Message
  
--- src/api/Chat/GetChat/GetChat.resolvers.ts
+++ src/api/Chat/GetChat/GetChat.resolvers.ts
@@ -10,7 +10,10 @@ const resolvers: Resolvers = {
         async (_, args: GetChatQueryArgs, { req }): Promise<GetChatResponse> => {
             const user: User = req.user;
             try {
-                const chat = await Chat.findOne({ id: args.chatId });
+                const chat = await Chat.findOne(
+                    { id: args.chatId },
+                    { relations: ["messages"] }
+                );
                 if (chat) {
                     if (chat.passengerId === user.id || chat.driverId === user.id) {
                         return {
